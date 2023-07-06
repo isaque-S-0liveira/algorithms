8bb1da5068301ceacab1b38e7dc7fb5f641cf81b
Isaque <zazac3179@gmail.com>
2023-07-06 16:08:36 -0300

    resolvendo problema de complexidade

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,9 +3,11 @@ def study_schedule(permanence_period, target_time):
         return None
 
     count = 0
+
     for period in permanence_period:
-        if not all(isinstance(value, int) for value in period):
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
             return None
+
         if period[0] <= target_time <= period[1]:
             count += 1
 
