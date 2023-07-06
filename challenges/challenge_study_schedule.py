08c2f8c48f87d7b0953d24fd65aa44a26815facd
Isaque <zazac3179@gmail.com>
2023-07-06 15:38:35 -0300

    implementação da função study_schedule que busca qual o melhor horário para disponibilizar os conteúdos de estudo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+
+    count = 0
+    for period in permanence_period:
+        if not all(isinstance(value, int) for value in period):
+            return None
+        if period[0] <= target_time <= period[1]:
+            count += 1
+
+    return count
