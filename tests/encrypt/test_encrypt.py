68d7736c0cbba998913197781e335fe53bc48688
Isaque <zazac3179@gmail.com>
2023-07-06 17:52:57 -0300

    implementação dos testes da função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,27 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError) as exc_info:
+        encrypt_message("message", "key")
+    assert str(exc_info.value) == "tipo inválido para key"
+
+    with pytest.raises(TypeError) as exc_info:
+        encrypt_message(3, 2)
+    assert str(exc_info.value) == "tipo inválido para message"
+
+    message = "xablau"
+    key = 7
+    encrypted_message = encrypt_message(message, key)
+    assert encrypted_message == "ualbax"
+
+    message = "xablau"
+    key = 2
+    encrypted_message = encrypt_message(message, key)
+    assert encrypted_message == "ualb_ax"
+
+    message = "xablau"
+    key = 3
+    encrypted_message = encrypt_message(message, key)
+    assert encrypted_message == "bax_ual"
