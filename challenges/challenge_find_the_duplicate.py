0248f107a351844620a54022e03486e4b2514ca9
Isaque <zazac3179@gmail.com>
2023-07-07 16:26:03 -0300

    implementação da função find_duplicate que retorna numeros duplicados em uma lista de inteiros

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,10 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) <= 1:
+        return False
+    nums.sort()
+    for i, n in enumerate(nums):
+        if isinstance(n, str) or n < 1:
+            return False
+        if i < len(nums) - 1 and n == nums[i + 1]:
+            return n
+    return False
